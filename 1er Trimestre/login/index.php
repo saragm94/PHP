e38ea09a9feec503b1ee54d6c9b047894cc22f22
2DAW10 <2DAW10@DOMDPTOINFJMPP.INF>
2022-10-21 16:02:05 +0200

    creado login con sesiones, arrays y limpia de inputs

--- 1er Trimestre/login/index.php
+++ 1er Trimestre/login/index.php
@@ -1,17 +1,22 @@
+<?php
+session_start(); 
+echo $_SESSION["usuario"];
+?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>User Login</title>
   </head>
   <body>
-  <form method="post" action="" name="signup-form">
+  <form method="post" action="login.php" name="login_inicio">
     <div class="form-element">
         <label>Username</label>
-        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+        <input type="text" name="usuario" required />
     </div>
     <div class="form-element">
         <label>Password</label>
-        <input type="password" name="password" required />
+        <input type="password" name="contra" required />
     </div>
     <button type="submit" name="register" value="register">Register</button>
 </form>
---
+++ 1er Trimestre/login/login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$usuario = $_POST["usuario"];
+$contra = $_POST["contra"];
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    limpiar($_POST["usuario"], $_POST["contra"]);
+}
+function limpiar($usuario, $contra) 
+{
+    $usuario = trim($usuario);
+    $usuario = stripslashes($usuario);
+    $usuario = htmlspecialchars($usuario);
+
+    $contra = trim($contra);
+    $contra = stripslashes($contra);
+    $contra = htmlspecialchars($contra);
+
+    global $usuario;
+    global $contra;
+
+    $validos['sara']="1234";
+    $validos['invitado'] = "12345";
+
+    foreach ($validos as $key => $value)
+    {
+        if($key == $usuario && $value == $contra)
+        {
+            echo "usuario y contraseñas correctos </br>";
+            $usuario_valido = $key;
+            $contra_valida = $contra;
+            $_SESSION['usuario']=$key;
+            $_SESSION['contrasena']=$contra;
+            header("Location:index.php");
+        }
+    }
+    
+}
+?>
